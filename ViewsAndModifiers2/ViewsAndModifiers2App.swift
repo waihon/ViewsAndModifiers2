e9669c8ec34791b492f475ef822947d23f19645f
Waihon Yew <yewwaihon@gmail.com>
2022-04-30 15:53:34 +0800

    Initial Commit

---
+++ ViewsAndModifiers2/ViewsAndModifiers2App.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiers2App.swift
+//  ViewsAndModifiers2
+//
+//  Created by Waihon Yew on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiers2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
