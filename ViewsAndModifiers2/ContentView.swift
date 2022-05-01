a41ba362fdff72836900794aed8bc043880e5279
Waihon Yew <yewwaihon@gmail.com>
2022-05-01 16:20:37 +0800

    Replace some View with a specific type of View

--- ViewsAndModifiers2/ContentView.swift
+++ ViewsAndModifiers2/ContentView.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    var body: Text {
+        Text("Hello, world!")
+    }
+}
+
+struct TextPaddingBackgroundContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
