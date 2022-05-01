f9758a4b3b9ce1f8b148c964e4c0f7453ef6344d
Waihon Yew <yewwaihon@gmail.com>
2022-05-01 17:04:32 +0800

    Apply conditional modifiers using regular if

--- ViewsAndModifiers2/ContentView.swift
+++ ViewsAndModifiers2/ContentView.swift
@@ -11,6 +11,24 @@ struct ContentView: View {
     @State private var useRedText = false
 
     var body: some View {
+        if useRedText {
+            Button("Hello World") {
+                useRedText.toggle()
+            }
+            .foregroundColor(.red)
+        } else {
+            Button("Hello World") {
+                useRedText.toggle()
+            }
+            .foregroundColor(.blue)
+        }
+    }
+}
+
+struct ConditionalModifiersTernaryContentView: View {
+    @State private var useRedText = false
+
+    var body: some View {
         Button("Hello World") {
             // Flip the Boolean between true and false
             useRedText.toggle()
