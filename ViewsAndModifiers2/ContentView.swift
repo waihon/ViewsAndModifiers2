077f2213e3e9ab3a9c003daa1a09ed3323adadca
Waihon Yew <yewwaihon@gmail.com>
2022-05-01 22:30:47 +0800

    Split up one large view into multiple smaller views

--- ViewsAndModifiers2/ContentView.swift
+++ ViewsAndModifiers2/ContentView.swift
@@ -7,8 +7,29 @@
 
 import SwiftUI
 
+struct CapsuleText: View {
+    var text: String
 
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
 struct ContentView: View {
+
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+        }
+    }
+}
+
+struct DuplicateModifiersContentView: View {
     var body: some View {
         VStack(spacing: 10) {
             Text("First")
