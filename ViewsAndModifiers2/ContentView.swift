144a543cb7d7b13b87f3b26560039a6741484832
Waihon Yew <yewwaihon@gmail.com>
2022-05-04 15:09:59 +0800

    Implement custom bindings for agreed to all

--- ViewsAndModifiers2/ContentView.swift
+++ ViewsAndModifiers2/ContentView.swift
@@ -7,6 +7,33 @@
 
 import SwiftUI
 
+struct ContentView: View {
+    @State private var agreedToTerms = false
+    @State private var agreedToPrivacyPolicy = false
+    @State private var agreedToEmails = false
+
+    var body: some View {
+        let agreedToAll = Binding<Bool>(
+            get: {
+                agreedToTerms && agreedToPrivacyPolicy && agreedToEmails
+            },
+            set: {
+                agreedToTerms = $0
+                agreedToPrivacyPolicy = $0
+                agreedToEmails = $0
+            }
+        )
+
+        return VStack {
+            Toggle("Agree to terms", isOn: $agreedToTerms)
+            Toggle("Agree to privacy policy", isOn: $agreedToPrivacyPolicy)
+            Toggle("Agree to receive shipping emails", isOn: $agreedToEmails)
+            Toggle("Agree to all", isOn: agreedToAll)
+        }
+        .padding()
+    }
+}
+
 struct ProminentTitle: ViewModifier {
     func body(content: Content) -> some View {
         content
@@ -39,7 +66,7 @@ struct GridStack<Content: View>: View {
     }
 }
 
-struct ContentView: View {
+struct GridContentView: View {
     var body: some View {
         Text("Grid Stack")
             .prominentTitle()
